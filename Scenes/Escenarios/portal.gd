e84e149cd8723b583720314d4022e024a1823d9b
pedrojos17 <pedrojoselorenteee@gmail.com>
2025-02-20 14:22:09 +0100

    portal y sonido de fondo
    portal y sonido de fondo

---
+++ Scenes/Escenarios/portal.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var send_ninja_to = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var portals = get_tree().get_nodes_in_group("portal")
+	for i in range (portals.size()):
+		if portals[i].position != position:
+			send_ninja_to = portals[i].position
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_teletransport_area_entered(area):
+	if area.get_parent().is_in_group("ninja"):
+		area.get_parent().position = send_ninja_to
